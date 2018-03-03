bac00586942069bde45479a64dba59fa3c28260e
mac-gallagher <jmgallagher36@gmail.com>
2018-03-02 21:41:26 -0600

    Apply refactor to reflect Protocol Oriented Programming (POP) (did not add new files in previous commit)

---
+++ Company-Contacts/Companies/CompaniesController+CreateCompanyControllerDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  CompaniesController+CreateCompanyControllerDelegate.swift
+//  Company-Contacts
+//
+//  Created by Mac Gallagher on 3/2/18.
+//  Copyright © 2018 Mac Gallagher. All rights reserved.
+//
+
+import UIKit
+
+extension CompaniesController: CreateCompanyControllerDelegate {
+    
+    func didAddCompany(company: Company) {
+        companies.append(company)
+        
+        let newIndexPath = IndexPath(row: companies.count - 1, section: 0)
+        tableView.insertRows(at: [newIndexPath], with: .automatic)
+    }
+    
+    
+    func didEditCompany(company: Company) {
+        
+        let row = companies.index(of: company)
+        
+        let reloadIndexPath = IndexPath(row: row!, section: 0)
+        tableView.reloadRows(at: [reloadIndexPath], with: .middle)
+    }
+    
+}
